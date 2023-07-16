a5cb6f5a97e2707efd769e4482a297bcc6d5592b
Thomas Dodds <thomasmd1928@gmail.com>
2023-07-16 16:35:20 -0500

    add non-interactive functionality
    test_id now allows for specifying input parameters and bypasses prompting the user if the parameter is set

--- lib/interactive.py
+++ lib/interactive.py
@@ -2,15 +2,19 @@
 
 #Assist functions for interactivity.
 
-def test_id(database, name):
-	while True:
-		test = ''
-		while test == '':
-			print('Enter ' + name + ':', end = ' ')
-			test = input().upper()
-			if test == '':
-				for i in database:
-					print(i.id + '\t' + i.name)
-		for i in database:
-			if i.id == test:
-				return i
+def test_id(database, name='query', input=None):
+	if input == None:
+		while True:
+			test = ''
+			while test == '':
+				print('Enter ' + name + ':', end = ' ')
+				test = input().upper()
+				if test == '':
+					for i in database:
+						print(i.id + '\t' + i.name)
+	else:
+		test = input.upper()
+	for i in database:
+		if i.id == test:
+			return i
+	return None

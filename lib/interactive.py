37e0b0efc5d08992928e1f4af896883ef0774920
Thomas Dodds <thomasmd1928@gmail.com>
2023-08-23 10:48:27 -0500

    add test_id description and usage

--- lib/interactive.py
+++ lib/interactive.py
@@ -2,7 +2,14 @@
 
 #Assist functions for interactivity.
 
-def test_id(database, name='query', test=''):
+#Search for a query in a database.
+#	database: a defined list of classes that contain an id element
+#	test: leave blank to prompt for input, otherwise, the id to search for
+#	name: when prompting for input, the proper name of the id to search for
+#
+#	pressing enter at a blank input prompt will list all the possible valid input ids
+#		(and corresponding names)
+def test_id(database, test='', name='query'):
 	while test == '':
 		print('Enter ' + name + ':', end = ' ')
 		test = input()

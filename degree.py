8a8126b2be5bd7a091fe4a662a31c92e23a4220c
kramkoob <thomasmd1928@gmail.com>
2023-10-27 00:17:43 -0500

    add semester schedules.
    actually using the semester class now. the accordion_to_degrees function calls the degree's populate_semesters which goes through a bit of magic to create the semesters. classes from the catalog aren't directly linked, but a searching implementation is provided as an example in the main().

--- degree.py
+++ degree.py
@@ -18,7 +18,7 @@
 #Prior to 2015: pdf format
 
 #from lib import cache, catalog
-import cache, catalog
+import cache, catalog, interactive
 
 import requests
 import bs4 as bs
@@ -28,13 +28,18 @@ _url_degree_list = r'https://www.atu.edu/catalog/current/undergraduate/programs.
 _url_root = r'https://www.atu.edu'
 
 class Semester():
-	def __init__(self, id, name):
-		self.name = name
-		self.id = id
-		self.entries = list()
-	def add_entry(self, entry):
-		if isinstance(entry, catalog.course):
-			self.entries.append(entry)
+	def __init__(self, year, season):
+		self.year = year
+		self.season = season
+		self.courses = []
+		self.others = []
+		self.hours = None
+	def add_course(self, name, id):
+		self.courses.append(name + ' ' + id)
+	def add_other(self, name):
+		self.others.append(name)
+	def set_hours(self, hours):
+		self.hours = hours
 
 class Degree():
 	def __init__(self, id, name, term):
@@ -42,17 +47,48 @@ class Degree():
 		self.id = id
 		self.semesters = []
 		self.term = term
-	def add_semester(self, name):
-		self.semesters.append(semester(len(self.semesters) + 1, name))
+	def _add_semester(self, year, season):
+		semester = Semester(year, season)
+		self.semesters.append(semester)
+		return semester
 	def add_track(self, n):
 		self.name = self.name + ' Track ' + str(n)
+	def populate_semesters(self, accordionelement):
+		# rip semesterly plan
+		cards = accordionelement.find_all('div', 'card')
+		for card in cards:
+			year = card.find('button', 'accordion-trigger').get_text()
+			tables = card.find_all('table')
+			for table in tables:
+				season = table.find('thead').find('th').get_text()
+				semester = self._add_semester(year, season)
+				course_containers = table.find_all('tr')
+				course_containers.pop(0) #remove first tr (header)
+				for course_container in course_containers:
+					try:
+						if course_container.find('strong').get_text() == "Total Hours":
+							semester.set_hours(course_container.find_all('td')[1].find('strong').get_text())
+					except AttributeError:
+						course_links = course_container.find_all('a')
+						for course_link in course_links:
+							course_name = course_link.get_text()
+							course_subject = course_name.split(' ')[0]
+							course_id = course_name.split(' ')[1]
+							try:
+								if course_subject.upper() == course_subject and str(int(course_id)) == course_id:
+									semester.add_course(course_subject, course_id)
+							except ValueError:
+								if course_id.count('X') > 2:
+									semester.add_other(course_name)
+								else:
+									print('unsure what to do with' + course_name)
 
-def _accordion_to_degrees(degreepage, degreelink, subjectIndex):
+def _accordion_to_degrees(degreepage, degreelink, term):
 	degreelist = []
 	track = 0
 	# find the div with accordion class
 	accordionelements = degreepage.find_all('div', 'accordion')
-	for _ in accordionelements:
+	for accordionelement in accordionelements:
 		# create degree
 		# name found from h1 header at top of page
 		degree_name = degreepage.find('div', 'col-lg-8 mb-5').find('h1').get_text()
@@ -63,6 +99,7 @@ def _accordion_to_degrees(degreepage, degreelink, subjectIndex):
 		if len(accordionelements) > 1:
 			track = track + 1
 			degree.add_track(track)
+		degree.populate_semesters(accordionelement)
 		degreelist.append(degree)
 	return degreelist
 
@@ -72,8 +109,6 @@ def degrees(term=catalog.terms()[0]):
 		degrees = cache.load(filename)
 	except FileNotFoundError:
 		print('Refreshing degrees, this may take some time...')
-		print('Semester courses will be populated using ' + term.name)
-		subjectIndex = catalog.subjects(term)
 		degrees = []
 		# load degree program page
 		degreeindex = bs.BeautifulSoup(requests.get(_url_degree_list).text, 'lxml')
@@ -86,7 +121,7 @@ def degrees(term=catalog.terms()[0]):
 		degreepages_texts = [requests.get(degreelink).text for degreelink in degreelinks]
 		degreepages = [bs.BeautifulSoup(degreepage_text, 'lxml') for degreepage_text in degreepages_texts]
 		for degreelink, degreepage in zip(degreelinks, degreepages):
-			degrees.extend(_accordion_to_degrees(degreepage, degreelink, subjectIndex))
+			degrees.extend(_accordion_to_degrees(degreepage, degreelink, term))
 		# create list of invalid pages
 		notdegreelinks = [degreelink for degreelink in degreelinks if degreelink not in [degree.id for degree in degrees]]
 		# try to find valid degrees in the remaining invalid pages
@@ -115,7 +150,7 @@ def degrees(term=catalog.terms()[0]):
 					degreepage_text = requests.get(testlink).text
 					degreepage = bs.BeautifulSoup(degreepage_text, 'lxml')
 					# test if a program exists in that page
-					degrees.extend(_accordion_to_degrees(degreepage, testlink, subjectIndex))
+					degrees.extend(_accordion_to_degrees(degreepage, testlink, term))
 				else:
 					break
 		# remove duplicates by link/id and name
@@ -132,5 +167,21 @@ def degrees(term=catalog.terms()[0]):
 
 if __name__ == '__main__':
 	print('degree.py')
-	degreelist = degrees()
 	
+	term = catalog.terms()[2]
+	degreelist = degrees(term = term)
+	subjects = catalog.subjects(term)
+	degree = degreelist[-1]
+	
+	print(degree.name)
+	for semester in degree.semesters:
+		print('\t' + semester.season + ' of ' + semester.year + ' year')
+		for course in semester.courses:
+			course_subject = course.split(' ')[0]
+			course_id = course.split(' ')[1]
+			subject = interactive.test_id(subjects, test = course_subject)
+			catcourses = catalog.courses(term, subject)
+			catcourse = interactive.test_id(catcourses, test = course_id)
+			print('\t\t' + catcourse.subject.name + ' ' + catcourse.id + ': ' + catcourse.name)
+		for other in semester.others:
+			print('\t\t' + other)

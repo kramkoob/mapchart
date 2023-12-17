8c88d05a24c8ae836b0e3e79d01ff91244440fa6
Thomas Dodds <thomasmd1928@gmail.com>
2023-12-17 16:25:30 -0600

    Integrate MakeCourseDescPdf files
    The sample course list (courselist.txt) and the template are now in the Python file. (NOTE: Untested)

--- MakeCourseDescPdf.py
+++ MakeCourseDescPdf.py
@@ -2,6 +2,48 @@
 
 #Create simple PDFs containing a course's information from the catalog
 
+'''
+Sample course list for Electrical Engineering:
+ENGL 1013
+CHEM 2124
+MATH 2914
+ELEG 1011
+TECH 1001
+ENGL 1023
+COMS 1011
+COMS 1013
+MATH 2924
+ELEG 2130
+ELEG 2134
+COMS 2203
+PHYS 2114
+PHYS 2000
+MATH 3243
+ELEG 2103
+ELEG 3133
+PHYS 2124
+MATH 2934
+ELEG 2111
+ELEG 2113
+STAT 3153
+ELEG 3003
+MCEG 3003
+ELEG 3103
+ELEG 3153
+ELEG 3123
+ELEG 3143
+ELEG 4103
+ELEG 4202
+MCEG 4202
+MATH 2703
+ELEG 4113
+ELEG 4143
+ELEG 4191
+ELEG 4303
+ELEG 4122
+ELEG 4192
+'''
+
 from lib import catalog
 from lib.interactive import test_id
 from sys import stdin
@@ -10,6 +52,18 @@ import pdfkit
 from jinja2 import Environment, FileSystemLoader
 
 _PATH_WKHTMLTOPDF = r'/usr/bin/wkhtmltopdf'
+_TEMPLATE = '''<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>{{ title }}</title>
+	</head>
+	<body>
+		<h2>{{ title }}</h2>
+		<p>{{ desc }}</p>
+	</body>
+</html>
+'''
 
 def main():
 	print('Make course description PDFs')
@@ -21,9 +75,11 @@ def main():
 		subjects = catalog.subjects(term)
 		print('done, using ' + term.name.upper())
 		
-		print('Init template...', end=' ')
-		jenv = Environment(loader=FileSystemLoader('.'))
-		jtemp = jenv.get_template("template.html")
+		print('Init template... ', end=' ')
+		#jenv = Environment(loader=FileSystemLoader('.'))
+		#jtemp = jenv.get_template("template.html")
+        jenv = Environment()
+        jtemp = jenv.from_string(_TEMPLATE)
 		print('done')
 
 		errors = 0
@@ -57,7 +113,7 @@ def main():
 				jhtml = jtemp.render(jtemp_vars)
 				pconfig = pdfkit.configuration(wkhtmltopdf=_PATH_WKHTMLTOPDF)
 				poptions = {
-					'page-size': 'A4',
+					'page-size': 'Letter',
 					'margin-top': '0.75in',
 					'margin-right': '0.75in',
 					'margin-bottom': '0.75in',
